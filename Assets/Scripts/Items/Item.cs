27c974447ae0fe8994057a5e0f293e64d2455d76
Harry Chew <harry@tech13.co.uk>
2025-04-13 16:36:23 +0100

    item constructor

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -8,5 +8,12 @@ namespace TPP.Scripts.Items
         public string itemName;
         public int weight;
         public int quantity;
+
+        public Item(string itemName, int weight, int quantity)
+        {
+            this.itemName = itemName;
+            this.weight = weight;
+            this.quantity = quantity;
+        }
     }
 }
