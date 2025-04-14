565b105a9e946af9f86b2f351753a3dcbaaa481b
Harry Chew <harry@tech13.co.uk>
2025-04-14 03:23:18 +0100

    simple daynight cycle

---
+++ Assets/Scripts/Day Night/DayNightCycle.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace TPP.Scripts.Systems
+{
+    [ExecuteAlways]
+    public class DayNightCycle : MonoBehaviour
+    {
+        [Header("References")]
+        public Light sun;
+        public LightingPreset preset;
+        
+        [Space(10)]
+        [Header("Variables")]
+        [Range(0f, 24f)]
+        public float timeOfDay;
+
+        private void UpdateLighting(float timePercent)
+        {
+            RenderSettings.ambientLight = preset.ambientColor.Evaluate(timePercent);
+            RenderSettings.fogColor = preset.fogColor.Evaluate(timePercent);
+
+
+            sun.color = preset.lightColor.Evaluate(timePercent);
+            sun.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+
+        private void Update()
+        {
+            if (preset == null)
+                return;
+
+            if (Application.isPlaying)
+            {
+                timeOfDay += Time.deltaTime;
+                timeOfDay %= 24f;
+                UpdateLighting(timeOfDay / 24f);
+            }
+            else
+            {
+                UpdateLighting(timeOfDay / 24f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Day Night/LightingPreset.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace TPP.Scripts.Systems
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptables/Lighting Preset", order = 1)]
+    public class LightingPreset : ScriptableObject
+    {
+        public Gradient ambientColor;
+        public Gradient lightColor;
+        public Gradient fogColor;
+    }
+}
